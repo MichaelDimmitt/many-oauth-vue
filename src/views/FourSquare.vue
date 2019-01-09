f260b5a344d0716f1efe1adb9ee7ae542b3713c8
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-01-09 18:05:25 -0500

    Foursquare login added.

--- src/views/FourSquare.vue
+++ src/views/FourSquare.vue
@@ -1,7 +1,9 @@
 <script>
-const MEETUP_CLIENT_ID = 'niqkagsfu07kb2coik832vljhm'; // this one is for local dev. http://127.0.0.1:3000
-const MEETUP_REDIRECT_URI = `http://${window.location.href.split('/')[2]}/home`;
+const FOURSQUARE_CLIENT_ID = 'SOTVCGCEZKOO3WRVWQ0Q4TWAFT2JCHFO4CAAATWEVXPS2HTY'; // this one is for local dev. http://127.0.0.1:3000
+const FOURSQUARE_REDIRECT_URI = `http://${
+  window.location.href.split('/')[2]
+}/home`;
 
-const url = `https://secure.meetup.com/oauth2/authorize?client_id=${MEETUP_CLIENT_ID}&response_type=token&redirect_uri=${MEETUP_REDIRECT_URI}`;
+const url = `https://foursquare.com/oauth2/authenticate?client_id=${FOURSQUARE_CLIENT_ID}&response_type=token&redirect_uri=${FOURSQUARE_REDIRECT_URI}`;
 window.location.href = url;
 </script>
