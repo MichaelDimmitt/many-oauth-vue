a50905deddd184895b43cd784729a8a153d709b3
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-01-09 17:57:31 -0500

    Provide a better home page

---
+++ src/components/HomeMessage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="message">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default { name: 'HomeMessage', props: { msg: String } };
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+    <HomeMessage msg="Cool, you must be logged in to the applications"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HomeMessage from '@/components/HomeMessage.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    HomeMessage,
   },
 };
 </script>
